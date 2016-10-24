1b912b562d02a6e1215abd23b4ec7f11613dd987
Mikelsr <mikelsr@opendeusto.es>
2016-10-24 12:47:37 +0200

    Fixed excesive line length

--- modules.py
+++ modules.py
@@ -19,14 +19,17 @@ def get_index():
     root_path = os.path.dirname(os.path.abspath(__file__))
     static_path = root_path + "/static"
 
-    # returns a list containing a json-structured representation of subsequent dirs and files
+    # returns a list containing a json-structured representation of subsequent
+    # dirs and files
     def get_scheme(x):
-        return [{y: get_scheme(x + "/" + y)} if os.path.isdir(x + "/" + y) else y for y in os.listdir(x)]
+        return [{y: get_scheme(x + "/" + y)} if os.path.isdir(x + "/" + y)
+                else y for y in os.listdir(x)]
 
     scheme["root"]["static"] = get_scheme(static_path)
-    # next line prettyprints the json
     # print(json.dumps(, indent=4, sort_keys=True))
     return json.dumps(scheme, ensure_ascii=False)
 
+
 if __name__ == "__main__":
-    print(json.dumps(json.loads(get_index(), encoding="utf-8"), indent=4, sort_keys=True))
+    print(print(json.dumps(json.loads(get_index(), encoding="utf-8"),
+                           indent=4, sort_keys=True)))
