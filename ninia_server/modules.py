f7e93edcdbea0295ab6eb9c72ae29a157f94b51d
Mikelsr <ewvem@localhost.localdomain>
2016-10-24 18:16:18 +0200

    Fixed and improved get_index module, reformatted html line length

--- ninia_server/modules.py
+++ ninia_server/modules.py
@@ -8,37 +8,31 @@ import json
 Returns json file containing the files and directories subsequent to the path
 passed as a parameter. Each folder has a list containing everything in of it.
 
-:param path: optional, default=/ninia_server/app/root/static/media/
+:param path: optional, default=root
 """
 
+
+# todo: ignore not permitted folders
 def get_index(path=""):
+
     if not path:
+        path = os.path.dirname(os.path.abspath(__file__))
+
+    def get_scheme(path):
         scheme = {
-            "root": {
-                "static": [
-
-                ]
-            }
+            "folders": [],
+            "files": []
         }
+        for x in os.listdir(path):
+            if os.path.isdir(path + "/" + x):
+                scheme["folders"].append({x: get_scheme(path + "/" + x)})
+            else:
+                scheme["files"].append(x)
 
-        # returns the path to the current file
-        # multiple variables are just for better understanding
-        root_path = os.path.dirname(os.path.abspath(__file__))
-        media_path = root_path + "/app/static/media/"
-        path = media_path
-    else:
-        scheme = {}
+        return scheme
 
-    # returns a list containing a json-structured representation of subsequent
-    # dirs and files
-    def get_scheme(x):
-        return [{y: get_scheme(x + "/" + y)} if os.path.isdir(x + "/" + y)
-                else y for y in os.listdir(x)]
-
-    scheme["root"]["static"] = get_scheme(path)
-    # print(json.dumps(, indent=4, sort_keys=True))
-    return json.dumps(scheme, ensure_ascii=False)
-
+    return json.dumps(get_scheme(path), ensure_ascii=False,
+                      indent=4, sort_keys=True)
 
 """
 Returns mimetype of the file given as a parameter
@@ -46,15 +40,12 @@ Returns mimetype of the file given as a parameter
 :param file
 """
 
+
 def get_type(file):
-
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    dict_path = root_path + "/app/dictionaries/"
-
     # Generate dictionaries
-    with open(dict_path + 'audio_dict.json', 'r') as fp:
+    with open('ninia_server/app/dictionaries/audio_dict.json', 'r') as fp:
         audio_dict = json.load(fp)
-    with open(dict_path + 'video_dict.json', 'r') as fp:
+    with open('ninia_server/app/dictionaries/video_dict.json', 'r') as fp:
         video_dict = json.load(fp)
 
     filename, extension = file.split(".")
@@ -71,10 +62,9 @@ def get_type(file):
 Autogenerates a menu with the media files stored in app/static/media/
 """
 
+
 def gen_menu():
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    static_path = root_path + "/app/"
-    menu_entries = os.listdir(static_path + "static/media/")
+    menu_entries = os.listdir("ninia_server/app/static/media")
     menu_audio = []
     menu_video = []
 
@@ -84,22 +74,35 @@ def gen_menu():
         if "video" in get_type(menu_entries[i]):
             menu_video.append(menu_entries[i])
 
-    menu_file = open(static_path + "templates/menu.html", "w")
+    menu_file = open("ninia_server/app/templates/menu.html", "w")
 
     # writes beginning section
-    menu_file.write("""<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>PyMediaServer - Menu</title>
-    </head><body><h1>PyMediaServer (in-dev) - Menu</h1>""")
+    menu_file.write("""<!DOCTYPE html><html lang="en">
+    <head><meta charset="UTF-8">
+    <title>PyMediaServer - Menu</title>
+    </head>
+    <body><h1>PyMediaServer (in-dev) - Menu</h1>""")
 
     # writes audio sectiom
     menu_file.write("<h2>Audio:</h2>")
     for i in range(len(menu_audio)):
-        menu_file.write('<a href="http://0.0.0.0:5000/play/' + menu_audio[i] + '">' + menu_audio[i] + '</a>')
+        menu_file.write('<a href="http://0.0.0.0:5000/play/' + menu_audio[i] +
+                        '">' + menu_audio[i] + '</a>')
         menu_file.write("<br>")
     # writes video section
     menu_file.write("<h2>Video:</h2>")
     for i in range(len(menu_video)):
-        menu_file.write('<a href="http://0.0.0.0:5000/play/' + menu_video[i] + '">' + menu_video[i] + '</a>')
+        menu_file.write('<a href="http://0.0.0.0:5000/play/' + menu_video[i] +
+                        '">' + menu_video[i] + '</a>')
         menu_file.write("<br>")
     # writes end section
     menu_file.write("</body></html>")
+
+
+if __name__ == "__main__":
+
+    # Test get_index() by printing the returned json of the root folder
+    def test_index(path=""):
+        print(json.dumps(json.loads(get_index(path), encoding="utf-8"),
+                         ensure_ascii=False, indent=4, sort_keys=True))
 
