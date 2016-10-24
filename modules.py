c571fe864187e472868e71b83dfa4edf03975b43
Mikelsr <mikelsr@opendeusto.es>
2016-10-24 13:50:51 +0200

    Added parameter passing and documentation

--- modules.py
+++ modules.py
@@ -4,25 +4,33 @@
 import os
 import json
 
-# returns a list containing a json-structured representation of subsequent
-# dirs and files
-"""
-todo:
-  take path passed as parameter as reference
-"""
-def get_index():
 
-    scheme = {
-        "root": {
-            "static": [
+"""
+Returns json file containing the files and directories subsequent to the path
+passed as a parameter. Each folder has a list containing everything in of it.
 
-            ]
+:param path: optional, default=root/static
+"""
+
+
+def get_index(path=""):
+
+    if not path:
+        scheme = {
+            "root": {
+                "static": [
+
+                ]
+            }
         }
-    }
 
-    # returns the path to the current file
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    static_path = root_path + "/static"
+        # returns the path to the current file
+        # multiple variables are just for better understanding
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        static_path = root_path + "/static"
+        path = static_path
+    else:
+        scheme = {}
 
     # returns a list containing a json-structured representation of subsequent
     # dirs and files
@@ -30,7 +38,7 @@ def get_index():
         return [{y: get_scheme(x + "/" + y)} if os.path.isdir(x + "/" + y)
                 else y for y in os.listdir(x)]
 
-    scheme["root"]["static"] = get_scheme(static_path)
+    scheme["root"]["static"] = get_scheme(path)
     # print(json.dumps(, indent=4, sort_keys=True))
     return json.dumps(scheme, ensure_ascii=False)
 
