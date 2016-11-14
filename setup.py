8bc4fca66101ddf64d3c2498388e23f7b7239368
RafaelRomon <rafaelromonsagredo@opendeusto.es>
2016-11-14 11:58:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup
 
-setup(name="NiniaServer", version="0.1",
+setup(name="SlothCloud", version="0.1",
       description="RESTful access media server made using Python 3 and Flask",
-      author="Carlos Lago, Rafael Romón, Mikel Solabarrieta",
-      author_email="""
-      carlos_lago2@protonmail.com;
-      mikelsolabarrieta@protonmail.com;
-      rafaelromonsagredo@protonmail.com""",
+      author="Ninia",
+      author_email="ninia@protonmail.com",
       url="https://github.com/Ninia/SlothCloud",
       install_requires=["flask", "Flask-AutoIndex"])
