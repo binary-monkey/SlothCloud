351c5a4880f3bbc079c22405d4898ec0d9ad246f
RafaelRomon <RafaelRomon@github.com>
2016-11-15 13:05:41 +0100

    Fixed missing ) ._.

--- slcl_server/run.py
+++ slcl_server/run.py
@@ -8,6 +8,6 @@ from utils import nt
 
 context = SSL.Context(SSL.SSLv23_METHOD)
 context.use_privatekey_file(nt(app_path + '/static/slcl.key'))
-context.use_certificate_file(nt(app_path + '/static/slcl.crt')
+context.use_certificate_file(nt(app_path + '/static/slcl.crt'))
 
-app.run(host=host, port=port, debug=False, ssl_context=context, threaded=True)
+app.run(host=host, port=port, ssl_context=context, threaded=True)
