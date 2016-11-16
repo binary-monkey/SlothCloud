24528e277543dbb9a818c0165db3603eca89522f
RafaelRomon <RafaelRomon@github.com>
2016-11-16 12:27:37 +0100

    It works :D (sort of...)

--- slcl_server/run.py
+++ slcl_server/run.py
@@ -3,11 +3,7 @@
 
 from app import app
 from app.config.constants import host, port, app_path
-from OpenSSL import SSL
 from utils import nt
 
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file(nt(app_path + '/static/slcl.key'))
-context.use_certificate_file(nt(app_path + '/static/slcl.crt'))
-
+context = (nt(app_path + '/static/slcl.crt'), nt(app_path + '/static/slcl.key'))
 app.run(host=host, port=port, ssl_context=context, threaded=True)
