82a285df9356f44d3aa5254e387236bc082934ee
RafaelRomon <RafaelRomon@github.com>
2016-11-16 13:14:49 +0100

    Minor changes

--- slcl_server/app/config/constants.py
+++ slcl_server/app/config/constants.py
@@ -2,7 +2,7 @@ import os
 
 
 host = "localhost"
-port = "12344"
+port = "5000"
 
 slcl_path = os.path.dirname(os.path.abspath(__file__)).replace(
     "/app/config" if os.name != "nt" else "\\app\\config", '')
--- slcl_server/gen_ssl.sh
+++ slcl_server/gen_ssl.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-openssl req \
-       -newkey rsa:2048 -nodes -keyout app/static/slcl.key \
-       -x509 -days 365 -out app/static/slcl.crt
+sudo openssl genrsa -des3 -out app/static/slcl.key 2048
+sudo openssl req -new -key app/static/slcl.key -out app/static/slcl.csr
+sudo cp app/static/slcl.key app/static/slcl.key.org
+sudo openssl rsa -in app/static/slcl.key.org -out app/static/slcl.key
+sudo openssl x509 -req -days 365 -in app/static/slcl.csr -signkey app/static/slcl.key -out app/static/slcl.crt
+rm app/static/slcl.key.org
+rm app/static/slcl.csr
