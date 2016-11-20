907792ce278149542d4da73626fa6312e92fd291
RafaelRomon <RafaelRomon@github.com>
2016-11-20 13:20:28 +0100

    Modified gen_ssl.sh

--- slcl_server/gen_ssl.sh
+++ slcl_server/gen_ssl.sh
@@ -5,4 +5,4 @@ sudo cp app/static/slcl.key app/static/slcl.key.org
 sudo openssl rsa -in app/static/slcl.key.org -out app/static/slcl.key
 sudo openssl x509 -req -days 365 -in app/static/slcl.csr -signkey app/static/slcl.key -out app/static/slcl.crt
 rm app/static/slcl.key.org
-rm app/static/slcl.csr
+rm app/static/slcl.csrgit
