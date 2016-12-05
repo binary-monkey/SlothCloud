4335b2572760012b68e3de0cbbcf06cca9ecdf3a
Mikelsr <mikelsolabarrieta@protonmail.com>
2016-12-05 13:38:32 +0100

    setup generates alias in posix

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# TODO: Mikel: Create desktop icon for linux. (and maybe Windows)
+# TODO: Carlos, Mikel, Rafa: Create desktop icon for linux. (and maybe Windows)
 
-from setuptools import setup
+from app.config.constants import app_path
 from gen_ssl import create_self_signed_cert
+from os import name
+from setuptools import setup
+from subprocess import Popen, PIPE
 
 setup(name="SlothCloud", version="0.1",
       description="RESTful access media server made using Python 3 and Flask",
@@ -12,5 +15,23 @@ setup(name="SlothCloud", version="0.1",
       author_email="ninia@protonmail.com",
       url="https://github.com/Ninia/SlothCloud",
       install_requires=["flask", "Flask-AutoIndex", "pyopenssl"])
+
+if name == "posix":
+    print("[*] Generating alias for slcl...")
+
+    try:
+        with open("~/.bashrc", 'a') as bashrc:
+            bashrc.write("alias slcl='%s/run.py'" % app_path)
+        print("[*] Done.")
+    except FileNotFoundError:
+        print("[*] Error. Couldn't create alias")
+
+    print("[*] updating terminal...")
+    out, err = Popen("source ~/.bashrc", shell=True,
+                     stdout=PIPE, stderr=PIPE).communicate()
+    if err:
+        print(err)
+    else:
+        print("[*] Done.")
 
 create_self_signed_cert()
